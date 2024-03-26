d2e13cbae8adea0e07de1c368024d1637a2bff42
Rohit <rohit016more@gmail.com>
2024-03-26 18:59:43 +0530

    GRID SECTION MODULE 3

--- qtify/src/api/api.js
+++ qtify/src/api/api.js
@@ -12,24 +12,24 @@ export const fetchTopAlbums = async() => {
         return null
     }
 }
-// export const fetchNewAlbums = async() => {
-//     try{
-//         const res = await axios.get(`${BACKEND_ENPOINT}/albums/new`);
-//         return res.data
-//     }
-//     catch(error){
-//         console.log(error)
-//         return null
-//     }
-// }
+export const fetchNewAlbums = async() => {
+    try{
+        const res = await axios.get(`${BACKEND_ENPOINT}/albums/new`);
+        return res.data
+    }
+    catch(error){
+        console.log(error)
+        return null
+    }
+}
 
-// export const fetchSongs = async() => {
-//     try{
-//         const res = await axios.get(`${BACKEND_ENPOINT}/songs`);
-//         return res.data
-//     }
-//     catch(error){
-//         console.log(error)
-//         return null
-//     }
-// }
+export const fetchSongs = async() => {
+    try{
+        const res = await axios.get(`${BACKEND_ENPOINT}/songs`);
+        return res.data
+    }
+    catch(error){
+        console.log(error)
+        return null
+    }
+}
