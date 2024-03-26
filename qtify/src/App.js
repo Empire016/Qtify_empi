fabc180abbf745fd6ec940f5382be36dcce89191
Rohit <rohit016more@gmail.com>
2024-03-26 18:21:08 +0530

    Grid Section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,50 @@
+import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
-import './App.css';
+import styles from './App.module.css'
+import { useEffect, useState } from 'react'
+import { fetchTopAlbums} from './api/api'
+import Section from './components/Section/Section.jsx'
+
 
 function App() {
+
+  const [topAlbumSongs, setTopAlbumSongs] = useState([])
+  const [value, ] = useState(0);
+
+  const generateTopAlbumSongs = async () => {
+    try {
+      const topAlbumSongs = await fetchTopAlbums()
+      setTopAlbumSongs(topAlbumSongs)
+    }
+    catch (error) {
+      console.log(error)
+      return null
+    }
+
+  }
+
+  useEffect(() => {
+    // eslint-disable-next-line
+  }, [value])
+
+  useEffect(() => {
+    
+    generateTopAlbumSongs();
+    // generateNewAlbumSongs();
+    // generateFilterSongs();
+    // setFilteredDataValues(newAlbumSongs);
+
+  }, [])
   return (
     <div>
        <Navbar />
        <Hero/>
+       <div className={styles.sectionWrapper}>
+        <Section type='album' title='Top Albums' data={topAlbumSongs} />
+        {/* <Section type='album' title='New Albums' data={newAlbumSongs} /> */}
+        {/* <FilterSection data={newAlbumSongs} type='songFilter' title='Songs' filteredData={filteredData} filteredDataValues={filteredDataValues} value={value} handleChange={handleChange} handleToggle={handleToggle}/> */}
+      </div>
     </div>
   );
 }
